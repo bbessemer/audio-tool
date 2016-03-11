94c6715083124e5b2524b351531722d1d0dc14d6
Brent Bessemer <bmb0610@gmail.com>
2016-03-11 11:13:21 -0500

    Added back a few semicolons that were actually necessary

--- musicmap.h
+++ musicmap.h
@@ -64,7 +64,7 @@ struct Note
 	Instrument** instrument;
 	slScalar volume;
 	*/
-}
+};
 
 Note* SpawnNote ();
 Note* SpawnHiddenNote ();
--- samples.h
+++ samples.h
@@ -3,7 +3,7 @@ struct Instrument
 {
 	slSound* sound;
 	char* name;
-}
+};
 float GetInstrumentSample (slBU inst_id, float freq, float offset);
 void LoadAllInstruments();
 slBU GetInstrumentID(char*);
