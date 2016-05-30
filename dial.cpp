a91b72e64cdd778eacf45b509ad1e316f347f34f
Brent Bessemer <brentbessemer@brents-macbook-pro.local>
2016-05-30 18:23:55 -0400

    Slightly more dial work

--- dial.cpp
+++ dial.cpp
@@ -1,7 +1,31 @@
+/*
+ * Portions of this file are copied with modifications from the original source
+ * of the Slice Engine by Seth Carter. Used by permission.
+ */
+
+#include <math.h>
+#define M_2PI (2.0*M_PI)
+
 #include <slice.h>
 #include "sliceexts.h"
 
 slxDial** slxDials;
+slBU slxDialCount;
+
+void slxDialProc (slBox* box)
+{
+  for (int i = 0; i < slxDialCount; i++)
+  {
+    slxDial* dial = slxDials[i];
+    if (dial->behind == box || dial->mark == box) slActiveSlider = dial;
+  }
+}
+
+void slxSetDialPos (slxDial* dial, slScalar to)
+{
+  if (!dial) return; // Saves an indent over enclosing the whole thing
+  
+}
 
 slxDial* slxCreateDial (slBox* back, slBox* mark, bool invert)
 {
@@ -9,8 +33,8 @@ slxDial* slxCreateDial (slBox* back, slBox* mark, bool invert)
   dial->back = back;
   dial->mark = mark;
   slider->vertical = slider->isbar = false;
-  back->onclick = slxDialProc_Back;
-  mark->onclick = slxDialProc_Mark;
+  back->onclick = slxDialProc;
+  mark->onclick = slxDialProc;
   slAddItemTolist(&slxDials, &slxDialCount, dial);
   dial->onchange = NULL;
   dial->invert = invert;
